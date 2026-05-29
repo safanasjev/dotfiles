17182d1055b0335cbec19f7d0a180d31f8ba1510
Saveli Afanasjev <safanasjev@tutamail.com>
2026-05-29 13:50:53 +0300

    Add .config/fish/completions/fzf_configure_bindings.fish Add .config/fish/completions/docker.fish Add .config/fish/completions/kubectl.fish Add .config/fish/completions/orbctl.fish Add .config/fish/completions/tailscale.fish Add .config/fish/conf.d/fish_frozen_theme.fish Add .config/fish/conf.d/fish_frozen_theme.fish.bak Add .config/fish/conf.d/fzf.fish Add .config/fish/conf.d/puffer_fish_key_bindings.fish Add .config/fish/conf.d/rustup.fish Add .config/fish/conf.d/sponge.fish Add .config/fish/config.fish Add .config/fish/fish_plugins Add .config/fish/functions/_fzf_configure_bindings_help.fish Add .config/fish/functions/_fzf_extract_var_info.fish Add .config/fish/functions/_fzf_preview_changed_file.fish Add .config/fish/functions/_fzf_preview_file.fish Add .config/fish/functions/_fzf_report_diff_type.fish Add .config/fish/functions/_fzf_report_file_type.fish Add .config/fish/functions/_fzf_search_directory.fish Add .config/fish/functions/_fzf_search_git_log.fish Add .config/fish/functions/_fzf_search_git_status.fish Add .config/fish/functions/_fzf_search_history.fish Add .config/fish/functions/_fzf_search_processes.fish Add .config/fish/functions/_fzf_search_variables.fish Add .config/fish/functions/_fzf_wrapper.fish Add .config/fish/functions/_puffer_fish_expand_bang.fish Add .config/fish/functions/_puffer_fish_expand_buck.fish Add .config/fish/functions/_puffer_fish_expand_dot.fish Add .config/fish/functions/_puffer_fish_expand_star.fish Add .config/fish/functions/_sponge_clear_state.fish Add .config/fish/functions/_sponge_on_exit.fish Add .config/fish/functions/_sponge_on_postexec.fish Add .config/fish/functions/_sponge_on_preexec.fish Add .config/fish/functions/_sponge_on_prompt.fish Add .config/fish/functions/_sponge_remove_from_history.fish Add .config/fish/functions/fzf_configure_bindings.fish Add .config/fish/functions/mkcd.fish Add .config/fish/functions/sponge_filter_failed.fish Add .config/fish/functions/sponge_filter_matched.fish Add .config/fish/fish_variables Add .config/fish/themes/tokyonight.theme

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,40 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+starship init fish | source
+
+## Abbreviations
+
+### obsidian
+abbr --add obs obsidian
+
+### eza
+abbr --add ls eza -F
+abbr --add ll eza -F -lh
+abbr --add la eza -F -alh
+
+### tmux
+abbr --add tks tmux kill-session -t
+abbr --add tls tmux ls
+abbr --add ta tmux a
+abbr --add tat tmux a -t
+abbr --add tns tmux new -s
+
+### chezmoi
+abbr --add chez chezmoi
+
+## Environment variables
+
+### For bashrc syntax (export VARIABLE=value) use (set -x VARIABLE_NAME value)
+set -x DYLD_LIBRARY_PATH /usr/local/lib
+
+### Set github username
+set -x GITHUB_USERNAME safanasjev
+
+### Locale
+set -x LC_CTYPE en_US.UTF-8
+set -x LC_ALL en_US.UTF-8
+
+### GPG
+set -x GPG_TTY (tty)
