c5135a09e8a4135d40c3a70d444a8df986370ad5
Saveli Afanasjev <safanasjev@tutamail.com>
2026-07-31 14:55:30 +0300

    Add .config/nvim/lua/plugins/debug.lua

---
+++ dot_config/nvim/lua/plugins/debug.lua
@@ -0,0 +1,104 @@
+return {
+  'mfussenegger/nvim-dap',
+  dependencies = {
+    -- Debugger UI
+    'rcarriga/nvim-dap-ui',
+
+    -- Required dependency for nvim-dap-ui
+    'nvim-neotest/nvim-nio',
+
+    -- Installs the debug adapters
+    'mason-org/mason.nvim',
+    'jay-babu/mason-nvim-dap.nvim',
+
+    -- Virtual text showing variable values
+    'theHamsta/nvim-dap-virtual-text',
+
+    -- Add standalone debuggers here
+  },
+  keys = {
+    { '<F4>', function() require('dap').restart() end,  desc = 'Debug: Restart' },
+    { '<F5>', function() require('dap').continue() end, desc = 'Debug: Start/Continue' },
+    {
+      '<F6>',
+      function()
+        require('dap').close()
+        require('dapui').close()
+      end,
+      desc = 'Debug: Stop',
+    },
+    { '<F10>',     function() require('dap').step_over() end,                                           desc = 'Debug: Step Over' },
+    { '<F11>',     function() require('dap').step_into() end,                                           desc = 'Debug: Step Into' },
+    { '<F12>',     function() require('dap').step_out() end,                                            desc = 'Debug: Step Out' },
+    { '<leader>b', function() require('dap').toggle_breakpoint() end,                                   desc = 'Debug: Breakpoint' },
+    { '<leader>B', function() require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ') end, desc = 'Debug: Conditional Breakpoint' },
+    -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
+    { '<F7>',      function() require('dapui').toggle() end,                                            desc = 'Debug: See last session result.' },
+  },
+  config = function()
+    local dap = require 'dap'
+    local dapui = require 'dapui'
+
+    require('mason-nvim-dap').setup {
+      -- Makes a best effort to setup the various debuggers with
+      -- reasonable debug configurations
+      automatic_installation = true,
+
+      -- You can provide additional configuration to the handlers,
+      -- see mason-nvim-dap README for more information
+      handlers = {},
+
+      ensure_installed = {
+        'delve',
+        'codelldb',
+        'python',
+        -- Add more debuggers here
+      },
+    }
+
+    -- Setup virtual text
+    require('nvim-dap-virtual-text').setup()
+
+    -- Dap UI setup
+    -- For more information, see |:help nvim-dap-ui|
+    ---@diagnostic disable-next-line: missing-fields
+    dapui.setup {
+      -- Set icons to characters that are more likely to work in every terminal.
+      --    Feel free to remove or use ones that you like more! :)
+      --    Don't feel like these are good choices.
+      icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+      ---@diagnostic disable-next-line: missing-fields
+      controls = {
+        icons = {
+          pause = '⏸',
+          play = '▶',
+          step_into = '⏎',
+          step_over = '⏭',
+          step_out = '⏮',
+          step_back = 'b',
+          run_last = '▶▶',
+          terminate = '⏹',
+          disconnect = '⏏',
+        },
+      },
+    }
+
+    -- Change breakpoint icons
+    vim.api.nvim_set_hl(0, 'DapBreak', { fg = '#FF5F57' })
+    vim.api.nvim_set_hl(0, 'DapStop', { fg = '#FEBC2D' })
+    local breakpoint_icons = vim.g.have_nerd_font
+        and { Breakpoint = '', BreakpointCondition = '', BreakpointRejected = '', LogPoint = '', Stopped = '' }
+        or { Breakpoint = '●', BreakpointCondition = '⊜', BreakpointRejected = '⊘', LogPoint = '◆', Stopped = '⭔' }
+    for type, icon in pairs(breakpoint_icons) do
+      local tp = 'Dap' .. type
+      local hl = (type == 'Stopped') and 'DapStop' or 'DapBreak'
+      vim.fn.sign_define(tp, { text = icon, texthl = hl, numhl = hl })
+    end
+
+    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+    dap.listeners.before.event_exited['dapui_config'] = dapui.close
+  end,
+
+  -- Set up standalone debbugers here
+}
